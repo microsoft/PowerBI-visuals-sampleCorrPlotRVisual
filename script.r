988057e9845ef24374b83ea0e15ae3c1f1873c90
Raanan Zvi <raananz@microsoft.com>
2016-09-26 16:40:25 +0300

    Added input validation to R script
    This code will filter out non numeric and constant columns, and will
    also validate that there are at least 2 rows and 2 columns.
    If the input is not valid, an error message will be shown to the user
    without failing the visual

--- script.r
+++ script.r
@@ -3,6 +3,29 @@ if (!exists("settings_method"))
     settings_method = "circle";
 }
 
+######### Validate Input #########
+
+errorText <- NULL
+#filter out non-numeric columns and constant columns
+correctColumn <- function(someColumn) { is.numeric(someColumn) && length(unique(someColumn)) > 1 }
+useColumns <- sapply(Values,correctColumn)
+if(sum(useColumns) < ncol(Values))
+  errorText <- "Filtered out non numeric columns and constant columns"
+
+Values <- as.data.frame(Values[,useColumns])
+
+columnCount <- ncol(Values)
+rowCount <- nrow(Values)
+
+if (rowCount < 2 || columnCount < 2) {
+    plot.new()
+    errorText<-paste(errorText, "Not enough input dimensions", sep="\n")
+    title(main = NULL, sub = errorText, outer = FALSE, col.sub = "gray50", cex.sub = 0.75)
+    quit()
+}
+
+######### Script #########
+
 library(corrplot)
 M <- cor(Values)
 corrplot(M, method=settings_method)
